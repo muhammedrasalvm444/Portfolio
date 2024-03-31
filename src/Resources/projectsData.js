b7443656ce8f68ebee1da1e19a56bf7f982f6eb8
muhammedrasalvm444 <145126883+muhammedrasalvm444@users.noreply.github.com>
2024-03-31 16:02:12 +0530

    Update projectsData.js

--- src/Resources/projectsData.js
+++ src/Resources/projectsData.js
@@ -4,14 +4,14 @@ const projectsData = [
     image: "./socialmedia.svg",
     description:
       "A portfolio is a compilation of academic and professional materials that exemplifies your beliefs, skills, qualifications, education, training, and experiences",
-    link: "/",
+    link: "/https://jobkarosite.netlify.app/",
   },
   {
     title: "Shey Hiring",
     image: "./jobs.svg",
     description:
       "An ecommerce application developed using react , node , redux and mongodb",
-    link: "/",
+    link: "/https://sheyhiring-now.netlify.app/",
   },
 ];
 
